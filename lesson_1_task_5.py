3f15f6654ac2dc6dd069c5740d7d14d7826e2a13
Yriy <41549026+Bagumg@users.noreply.github.com>
2020-02-21 14:08:22 +0700

    Delete lesson_1_task_5.py

--- lesson_1_task_5.py
+++
@@ -1,9 +0,0 @@
-# 5. Пользователь вводит две буквы.
-# Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-a = input()
-b = input()
-print(alphabet.index(a))
-print(alphabet.index(b))
-print(alphabet.index(b) - alphabet.index(a))
